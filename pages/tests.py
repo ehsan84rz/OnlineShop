a3d613c459a606030abbba256c0924ce19cd4630
ehsan <ehsan84rz@gmail.com>
2023-07-28 08:45:44 +0330

    tests for pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class PagesTest(TestCase):
+    def test_home_page_by_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_details(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
+
+    def test_about_us_page_by_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_details(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, 'About Us')
