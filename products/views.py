aec98cac94b3496f3e7e440005f79d8ddc4faec2
ehsan <ehsan84rz@gmail.com>
2023-07-29 17:38:14 +0330

    add ProductDetailView

--- products/views.py
+++ products/views.py
@@ -5,6 +5,13 @@ from .models import Product
 
 class ProductListView(generic.ListView):
     model = Product
-    template_name = 'products/products_list.html'
+    template_name = 'products/product_list.html'
     context_object_name = 'products'
     queryset = Product.objects.filter(active=True)
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
