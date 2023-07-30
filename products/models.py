9c8c429fc9d93b7a5fa4a16d15a1a6ba408a8f76
ehsan <ehsan84rz@gmail.com>
2023-07-30 20:14:53 +0330

    create ProductComment model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -7,6 +8,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
+    # avatar = models.ImageField(height_field=60, width_field=60, blank=True, null=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
@@ -16,3 +18,27 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
+
+
+class ProductComment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'Very Bad',),
+        ('2', 'Bad',),
+        ('3', 'Normal',),
+        ('4', 'Good',),
+        ('5', 'Perfect',),
+    ]
+
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=2000)
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    # buyer = models.BooleanField(default=True) -> This should be in CustomUser and a FK to here
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.text
