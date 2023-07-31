c62ee1d2a206d7b1f0e64643951dd991abcc35a7
ehsan <ehsan84rz@gmail.com>
2023-07-31 12:38:43 +0330

    create CommentCreateView, A separate view to saving created comments

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
-from .models import Product
+from .models import Product, Comment
 from .forms import CommentForm
 
 
@@ -21,3 +22,17 @@ class ProductDetailView(generic.DetailView):
         context['comment_form'] = CommentForm()
         return context
 
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+
+        product_id = int(self.kwargs['product_id'])  # <int:product_id> in urls.py -> path()
+        product = get_object_or_404(Product, id=product_id)
+        obj.product = product
+
+        return super().form_valid(form)
