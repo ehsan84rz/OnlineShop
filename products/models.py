44b5b7968bf16fc531133b3c518315fdc55fbb3e
ehsan <ehsan84rz@gmail.com>
2023-08-04 00:16:19 +0330

    implement media

--- products/models.py
+++ products/models.py
@@ -42,6 +42,7 @@ class Comment(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='comments')
     text = models.TextField(max_length=3000, verbose_name=_('comment text'))
     stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('What is your score?'))
+    image = models.ImageField(verbose_name=_('Product Image'), upload_to='product/product_cover/', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime_created'))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
