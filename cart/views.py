e080f977874c790dd4c6b94ae852ec3f38232fdf
ehsan <ehsan84rz@gmail.com>
2023-08-03 19:41:47 +0330

    make add_to_cart_view POST only

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
 from .cart import Cart
 from .forms import AddToCartProductForm
@@ -19,6 +20,7 @@ def cart_detail_view(request):
     })
 
 
+@require_POST
 def add_to_cart_view(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
