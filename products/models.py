e7faf15f3fe6000126e24108c268120b17427e61
ehsan <ehsan84rz@gmail.com>
2023-08-04 00:59:14 +0330

    implement images

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     price = models.PositiveIntegerField(default=0, verbose_name=_('price'))
     active = models.BooleanField(default=True)
     # avatar = models.ImageField(height_field=60, width_field=60, blank=True, null=True)
+    image = models.ImageField(verbose_name=_('Product Image'), upload_to='product/product_cover/', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime_created'))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
@@ -42,7 +43,6 @@ class Comment(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='comments')
     text = models.TextField(max_length=3000, verbose_name=_('comment text'))
     stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('What is your score?'))
-    image = models.ImageField(verbose_name=_('Product Image'), upload_to='product/product_cover/', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime_created'))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
