8b69701b4a64f306a59b7f82f2b3f8216e0814c4
ehsan <ehsan84rz@gmail.com>
2023-08-05 01:59:31 +0330

    update order_create_view

--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext as _
+# from django.contrib.auth import get_user_model
 
 from .forms import OrderForm
+from .models import OrderItem
+from cart.cart import Cart
 
 
 @login_required
 def order_create_view(request):
     order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('Your Cart is empty'))
+        return redirect('product_list')
 
     if request.method == 'POST':
-        order_form = OrderForm(request)
+        order_form = OrderForm(request.POST)
         if order_form.is_valid():
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+            cart.clear()
+            request.user.first_name = order_obj.user.first_name
+            request.user.last_name = order_obj.user.last_name
+            request.user.save()
+            messages.success(request, 'Your Order has successfully placed')
+
+    # request.user.email = get_user_model().user.email
     return render(request, 'orders/order_create.html', context={'form': order_form})
