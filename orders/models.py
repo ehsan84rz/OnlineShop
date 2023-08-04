6e29a7ed4f298a5d7e1529ab8dec0fd779db4999
ehsan <ehsan84rz@gmail.com>
2023-08-04 23:55:23 +0330

    create Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    is_paid = models.BooleanField(default=False, verbose_name=_('Is it Paid?'))
+    first_name = models.CharField(max_length=100, verbose_name=_('First Name'))
+    last_name = models.CharField(max_length=100, verbose_name=_('Last Name'))
+    phone_number = models.CharField(max_length=15, verbose_name=_('Phone Number'))
+    address = models.CharField(max_length=700, verbose_name=_('Address'))
+    order_notes = models.CharField(max_length=700, blank=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime_created'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
+
+    def __str__(self):
+        return f'Order: {self.id}'
