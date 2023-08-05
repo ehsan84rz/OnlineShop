76dd0618f3019b1bfbbd1f307ed69806d0d918f4
ehsan <ehsan84rz@gmail.com>
2023-08-05 09:37:19 +0330

    add translation and fix error

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .cart import Cart
 from .forms import AddToCartProductForm
@@ -30,8 +32,10 @@ def add_to_cart_view(request, product_id):
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
         cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
-
-    return redirect('cart:cart_detail')
+        return redirect('product_detail', product_id)
+    else:
+        messages.error(request, _('The entered value is not valid'))
+        return redirect('product_detail', product_id)
 
 
 def remove_from_cart_view(request, product_id):
